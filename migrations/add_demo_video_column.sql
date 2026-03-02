491173ad837a0f5806f8907d03ded2138cf11db7
Omar Rehan <ai.omar.rehan@gmail.com>
2026-03-02 17:39:58 +0400

    Improve Performance

--- migrations/add_demo_video_column.sql
+++
@@ -1,7 +0,0 @@
--- Add demo_video column to projects table
--- Run this SQL in your Supabase SQL Editor
-
-ALTER TABLE projects 
-ADD COLUMN IF NOT EXISTS demo_video TEXT;
-
-COMMENT ON COLUMN projects.demo_video IS 'Path to demo video stored in /public/demos folder';
